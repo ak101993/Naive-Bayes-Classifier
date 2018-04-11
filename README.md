31efba0696b83a720e78f582eef4d1c93df838a3
akshay <akshay.akskumar10@gmail.com>
2018-04-12 00:52:06 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Naive-Bayes-Classifier
+
+Naive -Bayes Classifer from scratch using python
+
+Spam Detection
+
+Company which provides email services to millions of people.Nowadays spam has a been a major problem and has caused your customers to leave. currently spam filter only filters out emails that have been previously marked as spam by your customers. However, spammers have become more clever and in order to stop your customers from leaving, you are given the assignment of predicting whether an email is spam.
+
+
+Bayes’ Theorem
+
+Bayes' theorem is a formula that describes how to update the probabilities of hypotheses when given evidence. It follows simply from the axioms of conditional probability, but can be used to powerfully reason about a wide range of problems involving belief updates.
+
+
+    P(SPAM|EMAIL)=(P(EMAIL|SPAM) * P(SPAM))/P(EMAIL)
+
+There are three functions :
+         probalityofWord
+         totalProbability
+         bagOfWordsClassification- used to create bag of words
---
+++ Spam_Detection.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+
+email_data=pd.read_csv('spam.csv',encoding='latin1')
+email_data=email_data[['v1','v2']]
+# print(email_data.head())
+# print(email_data[['v1']])
+
+# email_d=np.array(email_data[['v1']])
+# email_content=np.array(email_data[['v2']])
+
+
+
+#converts pandas dataframe to numpy array
+email_data=email_data.values
+print(email_data.shape)
+
+postive_spam=dict()
+negative_spam=dict()
+
+def bagOfWordsClassification(email_classification,email_content):
+    for word in email_content.split():
+        if email_classification=='spam':
+            postive_spam[word]=postive_spam.get(word,0)+1
+            global total_postive
+            total_postive+=1
+        else:
+            negative_spam[word]=negative_spam.get(word,0)+1
+            global total_negative
+            total_negative+=1
+    # print(postive_spam)
+    # print(negative_spam)
+total_postive=0
+total_negative=0
+total=0
+spam=0
+for email in email_data:
+    if email[0]=='spam':
+        spam+=1
+    total+=1
+    bagOfWordsClassification(email[0],email[1])
+print(total,'    ',spam)
+pA=spam/total
+notpA=(total-spam)/total
+
+
+def probalityofWord(spam,word):
+    if spam:
+        probaliltiy_postive=postive_spam[word]/total_postive
+        return probaliltiy_postive
+    else:
+        probaliltiy_negative=negative_spam[word]/total_negative
+        return probaliltiy_negative
+
+def totalProbability(spam,email_content):
+    total_probility=1
+    for word in email_content.split():
+        total_probility*=probalityofWord(spam,word)
+    return total_probility
+
+def classify(email):
+
+    isSpam=0
+    # isSpam=pA*totalProbability(True,email)
+    notSpam=notpA*totalProbability(False,email)
+
+    if isSpam>notSpam:
+        print('Spam')
+    else:
+        print('Not Spam')
+
+classify('watching')
+print(total_negative)
+print(total_postive)
